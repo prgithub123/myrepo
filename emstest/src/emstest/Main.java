c2955684a4f360728937a6c8b1fde48f782944f7
pawel.rakowski <p.rakowski@minto.pl>
2016-01-11 15:42:32 +0100

    przyklad aplikacji laczacej sie do EMS i wypisujacej kolejki

---
+++ emstest/src/emstest/Main.java
@@ -0,0 +1,31 @@
+package emstest;
+
+import com.tibco.tibjms.admin.QueueInfo;
+import com.tibco.tibjms.admin.TibjmsAdmin;
+import com.tibco.tibjms.admin.TibjmsAdminException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String serverUrl = "tcp://qxtemslb:7222";
+		String userName = "lukasz.chrzanowski";
+		String password = "lukasz.chrzanowski";
+		 
+		try {
+			TibjmsAdmin admin = new TibjmsAdmin(serverUrl, userName, password);
+			QueueInfo[] queues = admin.getQueues();
+			System.out.println("Kolejek: " + queues.length);
+			System.out.println("---");
+			
+			for (int i = 0; i < queues.length; i++) {
+				System.out.println(queues[i].getName() + ", pending msgs: " + queues[i].getPendingMessageCount());
+			}
+			 
+		} catch (TibjmsAdminException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
