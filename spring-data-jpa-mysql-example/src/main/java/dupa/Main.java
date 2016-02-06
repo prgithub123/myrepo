02381dc6e6e1941e2eca544e0b17b3042b191f76
pawel.rakowski <p.rakowski@minto.pl>
2016-02-06 22:35:56 +0100

    Dodanie properties w pliku

--- spring-data-jpa-mysql-example/src/main/java/dupa/Main.java
+++ spring-data-jpa-mysql-example/src/main/java/dupa/Main.java
@@ -8,13 +8,23 @@ public class Main {
 	public static void main(String[] args) {
 		AbstractApplicationContext context = new AnnotationConfigApplicationContext(PersistenceContext.class);
 		CustomerRepository repository = context.getBean(CustomerRepository.class);
-		repository.save(new Customer("Rajesh", "Bangalore"));
-		repository.save(new Customer("Krishna", "Kannur"));
-		System.out.println("CUSTOMER DETAILS");
-		Iterable<Customer> customers = repository.findAll();
+		
+		repository.save(new Customer("Adam", "Kowalski"));
+		repository.save(new Customer("Majek", "Srajek"));
+		Iterable<Customer> customers;
+		
+		System.out.println("findAll: ");
+		customers = repository.findAll();
 		for (Object customer : customers) {
 			System.out.println(customer);
 		}
+		
+		System.out.println("findByName:");
+		customers = repository.findByName("Majek");
+		for (Object customer : customers) {
+			System.out.println(customer);
+		}
+		
 		((AbstractApplicationContext) context).close();
 
 	}
--- spring-data-jpa-mysql-example/src/main/java/dupa/PersistenceContext.java
+++ spring-data-jpa-mysql-example/src/main/java/dupa/PersistenceContext.java
@@ -1,9 +1,13 @@
 package dupa;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -14,24 +18,44 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJpaRepositories
+@PropertySource({ "classpath:application.properties" })
 public class PersistenceContext {
+	
+	private static final String PROPERTY_NAME_DB_DRIVER_CLASS = "db.driver";
+    private static final String PROPERTY_NAME_DB_PASSWORD = "db.password";
+    private static final String PROPERTY_NAME_DB_URL = "db.url";
+    private static final String PROPERTY_NAME_DB_USER = "db.username";
+    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+    private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
+    private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
+    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+	
 	@Bean
-	public DataSource dataSource() {
+	public DataSource dataSource(Environment env) {
 		BasicDataSource ds = new org.apache.commons.dbcp.BasicDataSource();
-		ds.setDriverClassName("com.mysql.jdbc.Driver");
-		ds.setUrl("jdbc:mysql://localhost:3306/plktest");
-		ds.setUsername("root");
-		ds.setPassword("admin1");
+		ds.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DB_DRIVER_CLASS));
+		ds.setUrl(env.getRequiredProperty(PROPERTY_NAME_DB_URL));
+		ds.setUsername(env.getRequiredProperty(PROPERTY_NAME_DB_USER));
+		ds.setPassword(env.getRequiredProperty(PROPERTY_NAME_DB_PASSWORD));
 		return ds;
 	}
 
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
-			JpaVendorAdapter jpaVendorAdapter) {
+			JpaVendorAdapter jpaVendorAdapter, Environment env) {
 		LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
 		lef.setDataSource(dataSource);
 		lef.setJpaVendorAdapter(jpaVendorAdapter);
 		lef.setPackagesToScan("dupa");
+		
+		Properties jpaProperties = new Properties();
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+        jpaProperties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
+		lef.setJpaProperties(jpaProperties);
 		return lef;
 	}
 
